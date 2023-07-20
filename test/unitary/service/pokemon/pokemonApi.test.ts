a21f796d37ea670b8188a3fea9215b34e60e2249
Fernando Luppo <fernandoluppo2@gmail.com>
2023-07-20 18:51:31 -0300

    [27] fix: retirado mock desnecessário em pokemonApi.test.ts

--- test/unitary/service/pokemon/pokemonApi.test.ts
+++ test/unitary/service/pokemon/pokemonApi.test.ts
@@ -5,13 +5,54 @@ dotenv.config()
 describe("PokemonApi.ts", () => {
   describe("getAll", () => {
     it("Should call some pokemon and they basic information", async () => {
+      jest.setTimeout(10000)
       const url = new PokemonUrl()
       const api = new PokemonApi(url)
       const result = await api.getAll()
 
-      console.log(result)
+      expect(result.message).toBe("List of pokemons uploaded successfully")
+      expect(result.isError).toBe(false)
+      expect(result.data).toBeDefined()
+      expect(result.data.length).toBeGreaterThan(10)
+      expect(result.data[0]).toHaveProperty("type")
+      expect(result.data[0]).toHaveProperty("name")
+      expect(result.data[0]).toHaveProperty("id")
+      expect(result.data[0]).toHaveProperty("sprit")
     })
   })
-  describe("getOne", () => {})
-  describe("getEvolutions", () => {})
+  describe("getOne", () => {
+    it("Should call just one pokemon and he's information", async () => {
+      jest.setTimeout(10000)
+      const url = new PokemonUrl()
+      const api = new PokemonApi(url)
+      const result = await api.getOne(1)
+
+      expect(result.message).toBe("Pokemon uploaded successfully")
+      expect(result.isError).toBe(false)
+      expect(result.data).toBeDefined()
+      expect(result.data).toHaveProperty("pokemonMovie")
+      expect(result.data).toHaveProperty("pokemonStats")
+      expect(result.data).toHaveProperty("types")
+      expect(result.data).toHaveProperty("genericInfos")
+      expect(result.data).toHaveProperty("levels")
+      expect(result.data).toHaveProperty("description")
+    })
+  })
+  describe("getEvolutions", () => {
+    jest.setTimeout(10000)
+    it("Should return the evolution information of a pokemon", async () => {
+      const url = new PokemonUrl()
+      const api = new PokemonApi(url)
+      const result = await api.getEvolutions(1)
+
+      expect(result.message).toBe(
+        "Pokemon Evolution Chain uploaded successfully"
+      )
+      expect(result.isError).toBe(false)
+      expect(result.data).toBeDefined()
+      expect(result.data).toHaveProperty("types")
+      expect(result.data).toHaveProperty("genericInfos")
+      expect(result.data).toHaveProperty("levels")
+    })
+  })
 })
